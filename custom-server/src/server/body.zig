d0add7b7b9a1226e2b0fa23b8ba0373191a0f702
Casey <leptone55@gmail.com>
2026-05-18 17:20:14 -0400

    io_buf => read_buf

--- custom-server/src/server/body.zig
+++ custom-server/src/server/body.zig
@@ -3,11 +3,11 @@ const http_types = @import("http_types.zig");
 
 const Request = http_types.Request;
 
-pub fn read_request_body(allocator: std.mem.Allocator, req: *Request, max_bytes: usize, io_buf: []u8) ![]u8 {
+pub fn read_request_body(allocator: std.mem.Allocator, req: *Request, max_bytes: usize, reader_buf: []u8) ![]u8 {
     if (!req.head.method.requestHasBody()) {
         return try allocator.alloc(u8, 0);
     }
-    const body_reader = req.readerExpectNone(io_buf);
+    const body_reader = req.readerExpectNone(reader_buf);
     if (req.head.content_length) |len| {
         if (len > max_bytes) {
             return error.ContentTooLarge;
