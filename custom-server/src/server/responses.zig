101d8effd5422d8fa94204e008f906c86342fac2
Casey <leptone55@gmail.com>
2026-05-18 21:59:43 -0400

    cln

--- custom-server/src/server/responses.zig
+++ custom-server/src/server/responses.zig
@@ -3,23 +3,20 @@ const Response = http_types.Response;
 const Status = http_types.Status;
 
 pub fn not_found() Response {
-    return .{
-        .status = Status.not_found,
-        .headers = &.{.{
-            .name = "content_type",
-            .value = "text/plain",
-        }},
-        .body = "Not found",
-    };
+    return text(Status.not_found, "Not found");
 }
 
 pub fn bad_request(msg: []const u8) Response {
+    return text(Status.bad_request, msg);
+}
+
+pub fn text(status: Status, body: []const u8) Response {
     return .{
-        .status = Status.bad_request,
+        .status = status,
         .headers = &.{.{
-            .name = "content_type",
+            .name = "content-type",
             .value = "text/plain",
         }},
-        .body = msg,
+        .body = body,
     };
 }
--- custom-server/src/server/route_handlers.zig
+++ custom-server/src/server/route_handlers.zig
@@ -4,61 +4,32 @@ const Response = http_types.Response;
 const Status = http_types.Status;
 const HandlerCtx = http_types.HandlerCtx;
 const param = http_types.param;
+const resp = @import("responses.zig");
 
 pub fn get_root(_: HandlerCtx) !Response {
-    return .{
-        .status = Status.ok,
-        .headers = &.{
-            .{
-                .name = "content_type",
-                .value = "text/plain",
-            },
-        },
-        .body = "Welcome to the root",
-    };
+    return resp.text(Status.ok, "Welcome to the root");
 }
 
 pub fn get_name(_: HandlerCtx) !Response {
-    return .{
-        .status = Status.ok,
-        .headers = &.{
-            .{
-                .name = "content_type",
-                .value = "text/plain",
-            },
-        },
-        .body = "Casey",
-    };
+    return resp.text(Status.ok, "Casey");
 }
 
 pub fn get_user_age(ctx: HandlerCtx) !Response {
     const route_params = ctx.route_params;
 
-    return .{
-        .status = Status.ok,
-        .headers = &.{
-            .{
-                .name = "content_type",
-                .value = "text/plain",
-            },
-        },
-        .body = param(route_params, "age") orelse "missing",
-    };
+    return resp.text(
+        Status.ok,
+        param(route_params, "age") orelse "missing",
+    );
 }
 
 pub fn add_user(ctx: HandlerCtx) !Response {
-    return .{
-        .status = Status.created,
-        .headers = &.{
-            .{
-                .name = "content_type",
-                .value = "text/plain",
-            },
-        },
-        .body = try std.fmt.allocPrint(
+    return resp.text(
+        Status.created,
+        try std.fmt.allocPrint(
             ctx.allocator,
             "Created new user {s}",
             .{ctx.body},
         ),
-    };
+    );
 }
